6d55fef92e2ad652d2cc08f3f301acdb48a807ed
sbheemal <sbheemal@DIN73003167.corp.capgemini.com>
2018-11-23 14:57:26 +0530

    First commit.

---
+++ Project1/src/main/java/Sample1.java
@@ -0,0 +1,15 @@
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		System.out.println("Helloo11");
+		
+		greetings();
+
+	}
+
+	private static void greetings() {
+		System.out.println("Welcome");
+	}
+
+}
